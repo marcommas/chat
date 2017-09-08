e58de0dba63c2cd382993ea9e019fd02b4b9ac1f
marco <ti.marco@publicacoesonline.com.br>
2017-09-08 07:45:41 -0300

    incluindo conexão web socket

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 var application = require('./config/server');
 
 //parametrizar a porta que escuta
-application.listen(3000, function (){
+//var server = application.listen(3000, function (){
+var server = application.listen(80, function (){
     console.log('servidor online');
+});
+
+
+var io = require('socket.io').listen(server);
+
+//Criar a conexão por websocket
+io.on('connection', function(socket){
+    console.log('Usuário conectou socket');
+    
+    socket.on('disconnect', function(){
+       console.log('Usuário desconectou'); 
+    });
 });
